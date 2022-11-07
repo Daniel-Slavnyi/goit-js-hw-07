8b624bb9da979050105f3a4f682a278a076acdee
Daniel-Slavnyi <dslavnyj00@gmail.com>
2022-11-07 22:42:03 +0300

    add task2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const listOgPhoto = document.querySelector('.gallery');
 
+const photo = galleryItems.map(el => {
+    return `
+            <a class="gallery__item" href="${el.original}">
+                <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+            </a>
+           `;
+}).join('');
+
+listOgPhoto.innerHTML = photo;
+
+const onListOgPhotoClick = (e) => {
+    e.preventDefault();
+    const { target } = e;
+
+    if (target.nodeName !== "IMG") {
+        return;
+    }
+
+    const alt = e.target.alt;
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250
+    });
+    
+    listOgPhoto.removeEventListener('click', onListOgPhotoClick);
+};
+
+listOgPhoto.addEventListener('click', onListOgPhotoClick);
 console.log(galleryItems);
