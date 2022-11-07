7de85bf7403cf1ca252ec894c712f72cf6c06a94
Daniel-Slavnyi <dslavnyj00@gmail.com>
2022-11-07 20:25:58 +0300

    додає галерею

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,44 @@
+'use strict';
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const listOfImage = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const photo = galleryItems.map(el => {
+    return `
+    <li class="gallery__item">
+        <img src="${el.preview}" 
+             alt="${el.description}"
+             data-source="${el.original}" 
+             class="gallery__image">
+    </li>
+    `;
+}).join('');
+
+listOfImage.innerHTML = photo;
+
+const onListOfImageClick = (e) => {
+    const { target } = e;
+    if (target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${target.dataset.source}" width="800" height="600">`);
+
+    instance.show();
+
+    const onListOfImageKeydown = (e) => {
+
+        if (e.code === 'Escape') {
+            console.log(e.code);
+            instance.close();
+
+            window.removeEventListener('keydown', onListOfImageKeydown);
+        }
+    };
+
+    window.addEventListener('keydown', onListOfImageKeydown);
+};
+
+listOfImage.addEventListener('click', onListOfImageClick);
+
